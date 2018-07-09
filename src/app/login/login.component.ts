325f15c39383cf7578793647246130433f7a8fa6
azizhish <azizhish@gmail.com>
2018-07-09 17:24:01 +0400

    pipe problems when fetching users from restangular object.

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { AuthService } from '../services/auth.service';
+import { UserService } from '../services/user.service';
 
 
 
@@ -17,22 +18,25 @@ export class LoginComponent implements OnInit {
 
   constructor(
     private fb: FormBuilder,
-    private authservice:AuthService
-    ) {  }
+    private authservice: AuthService,
+    private userservice: UserService
+  ) { }
 
 
-  ngOnInit() {    
-      this.loginForm = this.fb.group({
+  ngOnInit() {
+    this.loginForm = this.fb.group({
       userName: ['', Validators.required],
       password: ['', Validators.required],
       remember: false
     });
+    this.userservice.getUserWithUsername('zaen').
+      subscribe(user => console.log(user.userPass));
   }
 
   onSubmit() {
     console.log("we get here");
-    if (this.loginForm.valid){
-      
+    if (this.loginForm.valid) {
+
       this.authservice.login(this.loginForm.value.userName, this.loginForm.value.password);
     }
     this.formSubmitAttempt = true;
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,17 +1,36 @@
 import { Injectable } from '@angular/core';
 import { Restangular } from 'ngx-restangular'
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { User } from "../../shared/user";
+import { map } from '../../../node_modules/rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
+  private userScope = new BehaviorSubject<User>(null);
+
   constructor(private restangular: Restangular) { }
 
-  getUser(userID: number): Observable<User> {
+  getUserWithID(userID: number): Observable<User> {
     return this.restangular.one('users', userID).get();
   }
+
+  getUserWithUsername(username: string): Observable<User> {
+    let x = this.restangular.all('users').getList().then(data => data.filter(user => user.userName === username))[0];
+    this.userScope.next(x);
+    return this.userScope;
+  }
+
+  getAllUsers() {
+    return this.restangular.all('users').getList();
+  }
+
+  test() {
+    this.getAllUsers().subscribe()
+  }
+
+
 
 }
